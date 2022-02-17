d12475a79f1df7919c69e50118de7011f1519352
Alin Popa <33693198+AlinPaul8806@users.noreply.github.com>
2022-02-17 23:26:21 +0200

    Darabase created

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Data
+{
+    //This is the bridge between your classes and the actual database 
+    public class DatabaseContext : DbContext
+    {
+        // initialize the base contructor to take the same options
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // it's here where we list out what the database should know about when it's been generated
+        // DbSet<DataType> = your table , make the entity plural if needed Countries = the name of the table in the db
+        // DbSet = a set of countries
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using HotelListing.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,8 +29,11 @@ namespace HotelListing
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
-
+            // add the dbcontext service: it will look in the appsettings.json
+            services.AddDbContext<DatabaseContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("sqlConnection"))
+            );
+            
             //adding CORS to the project, o = options
             // I will allow anybody to access my API
             services.AddCors(o => {
@@ -43,6 +48,8 @@ namespace HotelListing
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
             });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
